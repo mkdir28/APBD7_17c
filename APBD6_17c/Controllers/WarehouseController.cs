aa8b7effb2cb7aeed567d72889ddc9ce7637cc41
Maryna Kamienieva <mkdirwd@gmail.com>
2024-05-01 13:24:24 +0200

    Initial commit

---
+++ APBD6_17c/Controllers/WarehouseController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APBD7_17c.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WarehouseController : ControllerBase
+    {
+    }
+}
