dfd5a937888747925a011709acdab8a9f8498ab6
Maryna Kamienieva <mkdirwd@gmail.com>
2024-05-12 12:17:34 +0200

    update controller

--- APBD6_17c/Controllers/WarehouseController.cs
+++ APBD6_17c/Controllers/WarehouseController.cs
@@ -1,11 +1,22 @@
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using APBD7_17c.dto;
+using APBD7_17c.service;
+using Microsoft.Data.SqlClient;
 
-namespace APBD7_17c.Controllers
+namespace APBD7_17c.Controllers;
+[ApiController]
+[Route("api/[controller]")]
+public class WarehouseController(IWarehouseService service) : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class WarehouseController : ControllerBase
+
+    [HttpPost]
+    public async Task<IActionResult> CreateWarehouse(Product_Warehouse productWarehouse)
     {
+        var id = await service.CreatedRecord(productWarehouse);
+
+        return StatusCode(id == -1 ? StatusCodes.Status204NoContent : StatusCodes.Status201Created);
     }
+    
 }
+
